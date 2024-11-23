6c3ba4ad47ae1c6773c85c6da8f1d312a97cfabc
SifouAyad007 <71834798+SifouAyad007@users.noreply.github.com>
2024-11-23 13:36:00 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Slide from './components/Slide';
+
+function App() {
+  return (
+    <div className="App">
+    
+        
+    <Navbar/>
+
+ 
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './nav.css';
+
+function Navbar() {
+    return (
+
+      <div className="head">
+      <div className="menu">
+        <a href="" className="active"> home</a>
+        <a href=""> Pages</a>
+        <a href=""> Services</a>
+        <a href=""> Case Study</a>
+        <a href=""> Blog</a>
+        <a href=""> Contact </a>
+        </div>
+
+      </div>
+      
+    );
+  }
+  
+  export default Navbar;
